9369553b4c815e81f32771bae4c997ed73d62eb0
Tien Ly Thanh <career.tienlythanh@outlook.com>
2025-04-11 04:28:35 +0700

    add consumer for invitation queue
    add send invitation email function

--- backend/mail-service/services/mail.service.js
+++ backend/mail-service/services/mail.service.js
@@ -4,15 +4,6 @@ const nodemailer = require("nodemailer");
 const sendVerifyAccountEmail = async (
   data = { email: "", firstName: "", lastName: "", verifyURL: "" }
 ) => {
-  /** 
-  data: {
-    email: ``,
-    firstName: ``,
-    lastName: ``,
-    verifyUrl: ``,
-  }
-   */
-
   let transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
@@ -38,4 +29,46 @@ HCMUT Study Space Management Team
   return response;
 };
 
-module.exports = { sendVerifyAccountEmail };
+const sendJoinRoomInvitationEmail = async (
+  data = {
+    email: "",
+    invitationURL: "",
+    roomName: "",
+    roomType: "",
+    date: "",
+    startTime: "",
+    endTime: "",
+    inviterName: "",
+  }
+) => {
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASSWORD,
+    },
+    host: process.env.NODEMAILER_HOST,
+    port: process.env.NODEMAILER_PORT,
+  });
+
+  let response = await transporter.sendMail({
+    from: `HCMUT_RESERVATION_SERVICE <${process.env.NODEMAILER_USER}>`,
+    to: data.email,
+    subject: `Invitation to join study room at HCMUT`,
+    text: `You've received an invitation to join study room at HCMUT from ${data.inviterName}, with information below
+Room: ${data.roomName}
+Room Type: ${data.roomType}
+Date: ${data.date}
+From: ${data.startTime}
+To: ${data.endTime}
+
+Visit the link below to accept the invitation
+${data.invitationURL}
+HCMUT Study Space Management Team
+    `,
+  });
+
+  return response;
+};
+
+module.exports = { sendVerifyAccountEmail, sendJoinRoomInvitationEmail };
