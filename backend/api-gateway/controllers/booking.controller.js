0e516aefa841a5478d3162df8487d8220730c47f
Tien Ly Thanh <career.tienlythanh@outlook.com>
2025-04-11 04:25:27 +0700

    add handler for get booked  time slot API
    add handler for book time slot API
    
    add handler for invite to join room API
    
    add handler for accept invitation API

--- backend/api-gateway/controllers/booking.controller.js
+++ backend/api-gateway/controllers/booking.controller.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const client = require("../services/client.service");
+const jwt = require("jsonwebtoken");
+const { publishToQueue } = require("../services/rabbitmq.service");
+
+const RABBITMQ_EXCHANGE = process.env.RABBITMQ_EXCHANGE;
+const RABBITMQ_INVITATION_QUEUE = process.env.RABBITMQ_INVITATION_QUEUE;
+const RABBITMQ_INVITATION_ROUTE_KEY = process.env.RABBITMQ_INVITATION_ROUTE_KEY;
 
 const handleGetBookingHistory = async (req, res) => {
   const user = req["user"];
   var urlPath = req.originalUrl;
-  console.log(user);
 
   const APP_SERVICE_HOST = process.env.APP_SERVICE_HOST;
   const response = await client.get(`${APP_SERVICE_HOST}${urlPath}`, {
@@ -20,4 +26,127 @@ const handleGetBookingHistory = async (req, res) => {
   });
 };
 
-module.exports = { handleGetBookingHistory };
+const handleGetBookedTimeSlot = async (req, res) => {
+  var urlPath = req.originalUrl;
+
+  const APP_SERVICE_HOST = process.env.APP_SERVICE_HOST;
+  const response = await client.get(`${APP_SERVICE_HOST}${urlPath}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  res.status(response.status).json({
+    success: response.success,
+    data: response?.data,
+    message: response?.message,
+  });
+};
+
+const handleBookTimeSlot = async (req, res) => {
+  const user = req["user"];
+  var urlPath = req.originalUrl;
+
+  const APP_SERVICE_HOST = process.env.APP_SERVICE_HOST;
+  const response = await client.post(
+    `${APP_SERVICE_HOST}${urlPath}`,
+    { ...req.body },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        user: JSON.stringify(user),
+      },
+    }
+  );
+
+  res.status(response.status).json({
+    success: response.success,
+    data: response?.data,
+    message: response?.message,
+  });
+};
+
+const handleCreateInvitation = async (req, res) => {
+  const user = req["user"];
+  var urlPath = req.originalUrl;
+
+  const APP_SERVICE_HOST = process.env.APP_SERVICE_HOST;
+  const response = await client.post(
+    `${APP_SERVICE_HOST}${urlPath}`,
+    { ...req.body },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        user: JSON.stringify(user),
+      },
+    }
+  );
+
+  if (!response.success) {
+    return res.status(response.status).json({
+      success: response.success,
+      data: response?.data,
+      message: response?.message,
+    });
+  }
+
+  const invitation = response.data;
+  const participants = invitation.participants;
+
+  participants.forEach(async (email) => {
+    var token = jwt.sign(
+      {
+        secret: invitation.secret,
+        email: email,
+      },
+      process.env.JWT_TOKEN_SECRET
+    );
+
+    await publishToQueue(
+      RABBITMQ_EXCHANGE,
+      RABBITMQ_INVITATION_QUEUE,
+      RABBITMQ_INVITATION_ROUTE_KEY,
+      JSON.stringify({
+        email: email,
+        inviterName: invitation.inviterName,
+        roomName: invitation.roomName,
+        roomType: invitation.roomType,
+        date: invitation.date,
+        startTime: invitation.startTime,
+        endTime: invitation.endTime,
+        invitationURL: `${process.env.API_GATEWAY_HOST}/booking/invite/verify?token=${token}`,
+      })
+    );
+  });
+
+  res.status(response.status).json({
+    success: response.success,
+    data: response?.data,
+    message: response?.message,
+  });
+};
+
+const handleAcceptInvitation = async (req, res) => {
+  var urlPath = req.originalUrl;
+
+  const APP_SERVICE_HOST = process.env.APP_SERVICE_HOST;
+  const response = await client.get(`${APP_SERVICE_HOST}${urlPath}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  res.status(response.status).json({
+    success: response.success,
+    data: response?.data,
+    message: response?.message,
+  });
+};
+
+module.exports = {
+  handleGetBookingHistory,
+  handleGetBookedTimeSlot,
+  handleBookTimeSlot,
+  handleCreateInvitation,
+  handleAcceptInvitation,
+};
